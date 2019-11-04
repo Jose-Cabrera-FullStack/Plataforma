1520fe9fecf67f60c4352129033a1d249ee9e7e2
murdoc <murdoc.jose.6@gmail.com>
2019-11-04 01:21:10 -0300

    Se agrego la herramienta de Redux para analizar el estado

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import reducer from './reducers';
 
 
@@ -172,7 +172,7 @@ const initialState ={
           ]    
 }
 
-const store = createStore(reducer,initialState,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),);
+const store = createStore(reducer,initialState,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
        
