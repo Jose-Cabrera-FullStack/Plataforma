0e00bd61024e0e5953ee8c6b500c825b8bb195aa
murdoc <murdoc.jose.6@gmail.com>
2019-11-08 01:38:52 -0300

    FUNCIONA BIEN BIEN

--- src/frontend/actions/index.js
+++ src/frontend/actions/index.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 export const setFavorite = payload => ({
   type: 'SET_FAVORITE',
   payload,
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,7 @@
- require('@babel/register')({
-     ignore: [/(node_modeles)/],
-     presets: ['@babel/preset-env','@babel/preset-react'],
-     only: [__dirname + "/../../"],
-     ignore: [__dirname + "/../../**/node_modules"],
- });
-
- require('./server.js');
+require('@babel/register')({
+    ignore: [/(node_modules)/],
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+  });
+  
+  require('./server.js');
+  
