4fae4a3cc208c09b935745782821774471c84f2e
Cabrera Jose (Atencion a Usuarios) <mda_jcabrera@lnexternos.com.ar>
2020-01-06 01:16:58 -0300

    se esta pasando los datos del backend al state

--- src/server/routes/main.js
+++ src/server/routes/main.js
@@ -4,18 +4,19 @@ import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import {StaticRouter} from 'react-router';
 import {renderRoutes} from 'react-router-config';
+import axios from 'axios';
 import Routes from '../../frontend/routes/serverRoutes';
-// import Footer from '../../frontend/components/Footer';
 import reducer from '../../frontend/reducers';
 import render from '../render';
 
-const main = (req,res, next) => {
+require('dotenv').config();
+
+const main = async (req,res, next) => {
     try{
         let initialState;
         try{
-            const {email,name,id} = req.cookies;
+            const {token,email,name,id} = req.cookies;
             let user = {};
-
             if (email || name || id) {
                 user = {
                     id,
@@ -24,11 +25,25 @@ const main = (req,res, next) => {
                 };
             }
 
+            let courseList = await axios({
+                url: `${process.env.API_URL}/api/courses`,
+                headers: { Authorization: `Bearer ${token}` },
+                method: 'get',
+              });
+
+            courseList = courseList.data.data;
+
             initialState = {
                 user,
-                feature: {}
+                type: courseList.filter(course => course.type === 'Profesional Player'),
+                myList: courseList.filter(course => course._id === id),
             }
         }catch(error){
+            initialState = {
+                user:{},
+                type: {},
+                myList: [],
+            }
             console.log(error)
         }
         const store = createStore(reducer,initialState);
