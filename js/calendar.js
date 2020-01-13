376e9b09ccd2a25c9d6861995b151ce24c17019f
Cabrera Jose (Atencion a Usuarios) <mda_jcabrera@lnexternos.com.ar>
2020-01-13 03:11:46 -0300

    se esta comenzando a realizar el calendario

--- js/calendar.js
+++ js/calendar.js
@@ -1,81 +1,154 @@
-let today = new Date();
-let currentMonth = today.getMonth();
-let currentYear = today.getFullYear();
-let selectYear = document.getElementById("year");
-let selectMonth = document.getElementById("month");
-
-let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-let monthAndYear = document.getElementById("monthAndYear");
-showCalendar(currentMonth, currentYear);
-
-
-function next() {
-    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
-    currentMonth = (currentMonth + 1) % 12;
-    showCalendar(currentMonth, currentYear);
-}
-
-function previous() {
-    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
-    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
-    showCalendar(currentMonth, currentYear);
-}
-
-function jump() {
-    currentYear = parseInt(selectYear.value);
-    currentMonth = parseInt(selectMonth.value);
-    showCalendar(currentMonth, currentYear);
-}
-
-function showCalendar(month, year) {
-
-    let firstDay = (new Date(year, month)).getDay();
-    let daysInMonth = 32 - new Date(year, month, 32).getDate();
-
-    let tbl = document.getElementById("calendar-body"); // body of the calendar
-
-    // clearing all previous cells
-    tbl.innerHTML = "";
-
-    // filing data about month and in the page via DOM.
-    monthAndYear.innerHTML = months[month] + " " + year;
-    selectYear.value = year;
-    selectMonth.value = month;
-
-    // creating all cells
-    let date = 1;
-    for (let i = 0; i < 6; i++) {
-        // creates a table row
-        let row = document.createElement("tr");
-
-        //creating individual cells, filing them up with data.
-        for (let j = 0; j < 7; j++) {
-            if (i === 0 && j < firstDay) {
-                let cell = document.createElement("td");
-                let cellText = document.createTextNode("");
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-            }
-            else if (date > daysInMonth) {
-                break;
-            }
-
-            else {
-                let cell = document.createElement("td");
-                let cellText = document.createTextNode(date);
-                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
-                    cell.classList.add("bg-info");
-                } // color today's date
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-                date++;
-            }
-
-
-        }
-
-        tbl.appendChild(row); // appending each row into calendar body.
+class Calendar {
+    constructor(id) {
+        this.cells = [];
+        this.selectedDate = null;
+        this.currentMonth = moment();
+        this.elCalendar = document.getElementById(id);
+        this.showTemplate();
+        this.elGridBody = this.elCalendar.querySelector('.grid__body');
+        this.elMonthName = this.elCalendar.querySelector('.month-name');
+        this.showCells();
     }
 
+    showTemplate() {
+        this.elCalendar.innerHTML = this.getTemplate();
+        this.addEventListenerToControls();
+    }
+
+    getTemplate() {
+        let template = `
+            <div class="calendar__header">
+                <button type="button" class="control control--prev">&lt;</button>
+                <span class="month-name">dic 2019</span>
+                <button type="button" class="control control--next">&gt;</button>
+            </div>
+            <div class="calendar__body">
+                <div class="grid">
+                    <div class="grid__header">
+                        <span class="grid__cell grid__cell--gh">Lun</span>
+                        <span class="grid__cell grid__cell--gh">Mar</span>
+                        <span class="grid__cell grid__cell--gh">Mié</span>
+                        <span class="grid__cell grid__cell--gh">Jue</span>
+                        <span class="grid__cell grid__cell--gh">Vie</span>
+                        <span class="grid__cell grid__cell--gh">Sáb</span>
+                        <span class="grid__cell grid__cell--gh">Dom</span>
+                    </div>
+                    <div class="grid__body">
+
+                    </div>
+                </div>
+            </div>
+        `;
+        return template;
+    }
+
+    addEventListenerToControls() {
+        let elControls = this.elCalendar.querySelectorAll('.control');
+        elControls.forEach(elControl => {
+            elControl.addEventListener('click', e => {
+                let elTarget = e.target;
+                if (elTarget.classList.contains('control--next')) {
+                    this.changeMonth(true);
+                } else {
+                    this.changeMonth(false);
+                }
+                this.showCells();
+            });
+        });
+    }
+
+    changeMonth(next = true) {
+        if (next) {
+            this.currentMonth.add(1, 'months');
+        } else {
+            this.currentMonth.subtract(1, 'months');
+        }
+    }
+
+    showCells() {
+        this.cells = this.generateDates(this.currentMonth);
+        if (this.cells === null) {
+            console.error('No fue posible generar las fechas del calendario.');
+            return;
+        }
+
+        this.elGridBody.innerHTML = '';
+        let templateCells = '';
+        let disabledClass = '';
+        for (let i = 0; i < this.cells.length; i++) {
+            disabledClass = '';
+            if (!this.cells[i].isInCurrentMonth) {
+                disabledClass = 'grid__cell--disabled';
+            }
+            // <span class="grid__cell grid__cell--gd grid__cell--selected">1</span>
+            templateCells += `
+                <span class="grid__cell grid__cell--gd ${disabledClass}" data-cell-id="${i}">
+                    ${this.cells[i].date.date()}
+                </span>
+            `;
+        }
+        this.elMonthName.innerHTML = this.currentMonth.format('MMM YYYY');
+        this.elGridBody.innerHTML = templateCells;
+        this.addEventListenerToCells();
+    }
+
+    generateDates(monthToShow = moment()) {
+        if (!moment.isMoment(monthToShow)) {
+            return null;
+        }
+        let dateStart = moment(monthToShow).startOf('month');
+        let dateEnd = moment(monthToShow).endOf('month');
+        let cells = [];
+
+        // Encontrar la primer fecha que se va a mostrar en el calendario
+        while (dateStart.day() !== 1) {
+            dateStart.subtract(1, 'days');
+        }
+
+        // Encontrar la última fecha que se va a mostrar en el calendario
+        while (dateEnd.day() !== 0) {
+            dateEnd.add(1, 'days');
+        }
+
+        // Genera las fechas del grid
+        do {
+            cells.push({
+                date: moment(dateStart),
+                isInCurrentMonth: dateStart.month() === monthToShow.month()
+            });
+            dateStart.add(1, 'days');
+        } while (dateStart.isSameOrBefore(dateEnd));
+
+        return cells;
+    }
+
+    addEventListenerToCells() {
+        let elCells = this.elCalendar.querySelectorAll('.grid__cell--gd');
+        elCells.forEach(elCell => {
+            elCell.addEventListener('click', e => {
+                let elTarget = e.target;
+                if (elTarget.classList.contains('grid__cell--disabled') || elTarget.classList.contains('grid__cell--selected')) {
+                    return;
+                }
+                // Deselecionar la celda anterior
+                let selectedCell = this.elGridBody.querySelector('.grid__cell--selected');
+                if (selectedCell) {
+                    selectedCell.classList.remove('grid__cell--selected');
+                }
+                // Selecionar la nueva celda
+                elTarget.classList.add('grid__cell--selected');
+                this.selectedDate = this.cells[parseInt(elTarget.dataset.cellId)].date;
+                // Lanzar evento change
+                this.elCalendar.dispatchEvent(new Event('change'));
+            });
+        });
+    }
+
+    getElement() {
+        return this.elCalendar;
+    }
+
+    value() {
+        return this.selectedDate;
+    }
 }
