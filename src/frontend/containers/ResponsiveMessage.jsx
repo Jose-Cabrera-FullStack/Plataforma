1807c148faa0f1d3a4bae8a4201f222d48ff3926
Cova Luis (Atencion a Usuarios) <mda_lcova@lnexternos.com.ar>
2020-05-28 22:25:42 -0300

    se bloqueo el acceso de los dispositivos de menor dimension

--- src/frontend/containers/ResponsiveMessage.jsx
+++ src/frontend/containers/ResponsiveMessage.jsx
@@ -9,7 +9,7 @@ return (
         <div className="responsive__message">
             <h1 className="responsive__message__title">Entra desde un Computador</h1>
             <p className="responsive__message__text">ASÍ PODRAS ESCOGER EL DIA DE LA CLASE. TE ESPERAMOS</p>
-            <Link to="/home">
+            <Link to="/home"> 
                 <button className="btn__secondary btn__secondary--fix">
                     VUELVE AL INICIO
                 </button>
